c6d28055591c28f0a1382edc26094e295e3334dc
Leon Welchert <lichturm@posteo.de>
2025-07-05 00:09:17 +0200

    Write cluster-API provider generation script

---
+++ generate-cluster-api-providers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# https://github.com/kubernetes-sigs/cluster-api/releases/ 
+cluster_api_version="v1.9.6"
+# https://github.com/kubernetes-sigs/cluster-api-ipam-provider-in-cluster/releases/
+ipam_incluster_version="v1.0.2"
+# https://github.com/ionos-cloud/cluster-api-provider-proxmox/releases/
+capmox_version="v0.7.1"
+
+
+clusterctl generate provider --core  cluster-api:${cluster_api_version} --describe
+clusterctl generate provider --core  cluster-api:${cluster_api_version} > templates/provider-core/resources.yaml
+
+clusterctl generate provider --ipam in-cluster:${ipam_incluster_version} --describe 
+clusterctl generate provider --ipam in-cluster:${ipam_incluster_version} >  templates/provider-ipam/resources.yaml
+
+clusterctl generate provider --infrastructure proxmox:${capmox_version} --config secrets/clusterctl-config.yaml --describe
+clusterctl generate provider --infrastructure proxmox:${capmox_version} --config secrets/clusterctl-config.yaml > templates/provider-capmox/resources.yaml
