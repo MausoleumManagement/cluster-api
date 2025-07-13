33002e5449986a9fa3ed9a684bc4403e842e9e1a
Leon Welchert <lichturm@posteo.de>
2025-07-14 00:38:58 +0200

    add controlplane provider kubeadm to generation script

--- generate-cluster-api-providers.sh
+++ generate-cluster-api-providers.sh
@@ -7,14 +7,22 @@ ipam_incluster_version="v1.0.2"
 # https://github.com/ionos-cloud/cluster-api-provider-proxmox/releases/
 capmox_version="v0.7.1"
 
+# the documentation expects you to run `clusterctl init`, which has no option to output
+# manifests to stdout or anywhere other than straight onto the cluster
+# we use `clusterctl generate`, which works similarly, but acutally outputs the resources
+# the manifests clusterctl will download and apply are published on each release
+# https://github.com/kubernetes-sigs/cluster-api/releases
 
 clusterctl generate provider --core  cluster-api:${cluster_api_version} --describe
 clusterctl generate provider --core  cluster-api:${cluster_api_version} > templates/provider-core/resources.yaml
 
-# currently, this provider lives in the cluster-api repo
+# currently, these providers live in the cluster-api repo
 # https://github.com/kubernetes-sigs/cluster-api/tree/main/bootstrap/kubeadm
 clusterctl generate provider --bootstrap  kubeadm:${cluster_api_version} --describe
 clusterctl generate provider --bootstrap  kubeadm:${cluster_api_version} > templates/provider-bootstrap-kubeadm/resources.yaml
+# https://github.com/kubernetes-sigs/cluster-api/tree/main/controlplane/kubeadm
+clusterctl generate provider --control-plane  kubeadm:${cluster_api_version} --describe
+clusterctl generate provider --control-plane  kubeadm:${cluster_api_version} > templates/provider-controlplane-kubeadm/resources.yaml
 
 clusterctl generate provider --ipam in-cluster:${ipam_incluster_version} --describe 
 clusterctl generate provider --ipam in-cluster:${ipam_incluster_version} >  templates/provider-ipam/resources.yaml
