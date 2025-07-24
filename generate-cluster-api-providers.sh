80542e2394fbca70d146c5335b580e98ba0e9530
Leon Welchert <lichturm@posteo.de>
2025-07-24 21:43:24 +0200

    Configure custom renovate manager for cluster api provider versions

--- generate-cluster-api-providers.sh
+++ generate-cluster-api-providers.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+
+# the comments next to versions are for renovate, pls keep
+# the regex is rather pedantic, so make sure the layout stays the same
+# see: renovate.json5
+
 # https://github.com/kubernetes-sigs/cluster-api/releases/ 
-cluster_api_version="v1.9.6"
+CLUSTER_API_VERSION="1.9.6" # github-releases/kubernetes-sigs/cluster-api&versioning=semver
+
 # https://github.com/kubernetes-sigs/cluster-api-ipam-provider-in-cluster/releases/
-ipam_incluster_version="v1.0.2"
+IPAM_INCLUSTER_VERSION="1.0.2" # github-releases/kubernetes-sigs/cluster-api-ipam-provider-in-cluster&versioning=semver
+
 # https://github.com/ionos-cloud/cluster-api-provider-proxmox/releases/
-capmox_version="v0.7.1"
+CAPMOX_VERSION="0.7.1" # github-releases/ionos-cloud/cluster-api-provider-proxmox&versioning=semver
 
 # the documentation expects you to run `clusterctl init`, which has no option to output
 # manifests to stdout or anywhere other than straight onto the cluster
@@ -13,19 +20,19 @@ capmox_version="v0.7.1"
 # the manifests clusterctl will download and apply are published on each release
 # https://github.com/kubernetes-sigs/cluster-api/releases
 
-clusterctl generate provider --core  cluster-api:${cluster_api_version} --describe
-clusterctl generate provider --core  cluster-api:${cluster_api_version} > ./components/provider-core/resources.yaml
+clusterctl generate provider --core  cluster-api:v${CLUSTER_API_VERSION} --describe
+clusterctl generate provider --core  cluster-api:v${CLUSTER_API_VERSION} > ./components/provider-core/resources.yaml
 
 # currently, these providers live in the cluster-api repo
 # https://github.com/kubernetes-sigs/cluster-api/tree/main/bootstrap/kubeadm
-clusterctl generate provider --bootstrap  kubeadm:${cluster_api_version} --describe
-clusterctl generate provider --bootstrap  kubeadm:${cluster_api_version} > ./components/provider-bootstrap-kubeadm/resources.yaml
+clusterctl generate provider --bootstrap  kubeadm:v${CLUSTER_API_VERSION} --describe
+clusterctl generate provider --bootstrap  kubeadm:v${CLUSTER_API_VERSION} > ./components/provider-bootstrap-kubeadm/resources.yaml
 # https://github.com/kubernetes-sigs/cluster-api/tree/main/controlplane/kubeadm
-clusterctl generate provider --control-plane  kubeadm:${cluster_api_version} --describe
-clusterctl generate provider --control-plane  kubeadm:${cluster_api_version} > ./components/provider-controlplane-kubeadm/resources.yaml
+clusterctl generate provider --control-plane  kubeadm:v${CLUSTER_API_VERSION} --describe
+clusterctl generate provider --control-plane  kubeadm:v${CLUSTER_API_VERSION} > ./components/provider-controlplane-kubeadm/resources.yaml
 
-clusterctl generate provider --ipam in-cluster:${ipam_incluster_version} --describe 
-clusterctl generate provider --ipam in-cluster:${ipam_incluster_version} >  ./components/provider-ipam/resources.yaml
+clusterctl generate provider --ipam in-cluster:v${IPAM_INCLUSTER_VERSION} --describe 
+clusterctl generate provider --ipam in-cluster:v${IPAM_INCLUSTER_VERSION} >  ./components/provider-ipam/resources.yaml
 
-clusterctl generate provider --infrastructure proxmox:${capmox_version} --config clusterctl-config.yaml --describe
-clusterctl generate provider --infrastructure proxmox:${capmox_version} --config clusterctl-config.yaml > ./components/provider-capmox/resources.yaml
+clusterctl generate provider --infrastructure proxmox:v${CAPMOX_VERSION} --config clusterctl-config.yaml --describe
+clusterctl generate provider --infrastructure proxmox:v${CAPMOX_VERSION} --config clusterctl-config.yaml > ./components/provider-capmox/resources.yaml
